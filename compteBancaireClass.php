e3f435fe9c33f68ea0b90299c90fe341c25685ad
MaxRichet <max.richet@my-digital-school.org>
2023-10-12 17:00:25 +0200

    TP2

---
+++ compteBancaireClass.php
@@ -0,0 +1,24 @@
+<?php
+
+class CompteBancaire {
+    private $name;
+    private $value;
+
+    function __construct($name, $value) {
+        $this->name = $name;
+        $this->value = $value;
+    }
+
+    public function depot($somme) {
+        return $this->value + $somme;
+    }
+
+    public function retrait($somme) {
+        return $this->value - $somme;
+    }
+
+    public function affiche() {
+        return "Le solde de {$this->name}, est de {$this->value} euros";
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'compteBancaireClass.php';
+
+$name = 'Dupont';
+$value = 1000;
+
+$compte1 = new CompteBancaire('Duchmol', 800);
+$compte2 = new CompteBancaire($name, $value);
+
+$compte1->depot(350);
+$compte1->retrait(200);
+
+$compte2->depot(25);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compte Bancaire</title>
+</head>
+<body>
+    <p><?php $compte1->affiche(); ?></p><br>
+    <p><?php $compte2->affiche(); ?></p>
+</body>
+</html>
